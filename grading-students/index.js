7efb38ae382e83a0507459c31bd83f2574a88223
Bushra <bushranaeem941@gmail.com>
2025-02-14 13:06:34 +0500

    Fixed some code issues

--- grading-students/index.js
+++ grading-students/index.js
@@ -1,10 +1,10 @@
 function gradingStudents(grades) {
-  return grades.slice(1).map((grade) => {
+  return grades.map((grade) => {
     if (grade < 38) return grade;
     let multipleOf5 = Math.ceil(grade / 5) * 5;
     return multipleOf5 - grade < 3 ? multipleOf5 : grade;
   });
 }
-grades = [4, 73, 67, 38, 33];
+grades = [73, 67, 38, 33];
 const result = gradingStudents(grades);
 console.log(result);
